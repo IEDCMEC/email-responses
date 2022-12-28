87e4f1a490b4a9a49ac643c01d4fc7d08fe8a6dc
Aldrin Jenson <aldrinjenson@gmail.com>
2022-12-28 18:53:03 +0530

    Update script

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const speakerEmailRecipients = ["aldrinjenson@gmail.com"];
-const sponsorEmailRecipients = ["aldrinjenson@gmail.com"];
-const primaryEmailsList = ["aldrinjenson@gmail.com"];
+const speakerEmailRecipients = [
+  "annamma, sudeepasdfasdfasdfsdafdsf@gmail.com",
+  "aldrinjenson@gmail.com",
+];
+const sponsorEmailRecipients = [
+  "sreehari1111111111111@gmail.com",
+  "aldrinjenson@gmail.com",
+];
+const primaryEmailsList = [
+  "iedcmec1111111@mec.ac.in",
+  "aldrinjenson@gmail.com",
+];
 
 const urlParams = new URLSearchParams(window.location.search);
 const state = urlParams.get("state");
 const personName = urlParams.get("name");
-const mailType = urlParams.get("type");
+const mailType = urlParams.get("type") || "";
 const mailDate = urlParams.get("mailDate");
 const pocEmail = urlParams.get("poc");
 const currDate = new Date().toLocaleString();
@@ -22,17 +31,17 @@ const obj = {
 async function sendMail(toAll = false) {
   let toEmailList;
   if (toAll) {
-    if (mailType === "sponsor") {
-      toEmailList = speakerEmailRecipients;
-    } else {
+    if (mailType?.toLoweCase() === "sponsor") {
       toEmailList = sponsorEmailRecipients;
+    } else {
+      toEmailList = speakerEmailRecipients;
     }
     toEmailList.push(pocEmail);
   } else {
     toEmailList = primaryEmailsList;
   }
   console.log(toEmailList);
-  return;
+
   const url =
     "https://w2e9j471i2.execute-api.ap-south-1.amazonaws.com/dev/send-email";
   const text = Object.entries(obj).reduce(
